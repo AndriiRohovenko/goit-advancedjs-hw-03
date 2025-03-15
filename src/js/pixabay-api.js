24e296baaafe86af309967d1b305165913076b0e
Andrii.Rohovenko <andrii.rohovenko@gmail.com>
2025-03-15 17:47:34 +0200

    proceed with hw, add api component

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,3 +1,25 @@
-export function toCheck() {
-  console.log('js 1');
+const API_BASE_URL = 'https://pixabay.com/api/';
+// need to remove it to env variables or secured storage
+export const API_KEY = '49355059-ef28fa3d6cd8d2c420ac797e4';
+
+export function fetchSearchData(request_options) {
+  const queryParams = new URLSearchParams(request_options).toString();
+  const request_url = `${API_BASE_URL}?${queryParams}`;
+  return fetch(request_url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      return response.json();
+    })
+    .then(data => {
+      if (!data.hits || data.hits.length === 0) {
+        throw new Error(
+          'Sorry, there are no images matching your search query. Please try again!'
+        );
+      }
+      return data.hits;
+    })
+    .catch(error => console.error('Error fetching data:', error));
 }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
-import { toCheck as check1 } from './js/pixabay-api';
+import { fetchSearchData, API_KEY } from './js/pixabay-api';
 import { toCheck as check2 } from './js/render-functions';
 
-console.log(check1);
-console.log(check2);
+const search_form = document.getElementById('search-form');
+search_form.addEventListener('submit', search_image);
+
+function search_image(event) {
+  event.preventDefault();
+
+  const search_phrase = event.currentTarget.elements.query.value;
+  console.log(search_phrase);
+
+  const request_options = {
+    key: API_KEY,
+    q: search_phrase,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  };
+
+  const promise_value = fetchSearchData(request_options).then(data => {
+    console.log(data);
+  });
+  console.log(promise_value);
+}
