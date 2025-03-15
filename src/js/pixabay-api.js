f6e24f00d00f725fede105bf969a817cd66eb58b
Andrii.Rohovenko <andrii.rohovenko@gmail.com>
2025-03-15 14:47:07 +0200

    prepered a project structure, testing modules

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,3 @@
+export function toCheck() {
+  console.log('js 1');
+}
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { toCheck as check1 } from './js/pixabay-api';
+import { toCheck as check2 } from './js/render-functions';
+
+console.log(check1);
+console.log(check2);
