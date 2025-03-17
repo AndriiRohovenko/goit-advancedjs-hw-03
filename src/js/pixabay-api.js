ff1e3c6105de3dde1a6d3c94ccc25959b3fac413
Andrii.Rohovenko <andrii.rohovenko@gmail.com>
2025-03-17 12:21:50 +0200

    removed un-needed than from promise

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,9 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import axios from 'axios';
 
-const API_BASE_URL = 'https://pixabay.com/api/';
+axios.defaults.baseURL = `https://pixabay.com/api/`;
+
 // need to remove it to env variables or secured storage
 export const API_KEY = '49355059-ef28fa3d6cd8d2c420ac797e4';
 
@@ -14,23 +16,17 @@ export function fetchSearchData(search_phrase) {
     safesearch: 'true',
   };
   const queryParams = new URLSearchParams(request_options).toString();
-  const request_url = `${API_BASE_URL}?${queryParams}`;
-  return fetch(request_url)
+  const request_url = `?${queryParams}`;
+  return axios
+    .get(request_url)
     .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-
-      return response.json();
-    })
-    .then(data => {
-      if (!data.hits || data.hits.length === 0) {
+      if (!response.data.hits || response.data.hits.length === 0) {
         const errorText =
           '❌ Sorry, there are no images matching your search query. Please try again!';
         showError(errorText);
         throw new Error(errorText);
       }
-      return data.hits;
+      return response.data.hits;
     })
     .catch(error => console.error('Error fetching data:', error));
 }
